9b530e9106beb9e29d1b0b695873ca93809c4b6b
rickzeppelin <rfalcetti@gmail.com>
2024-03-14 17:03:52 -0300

    Update script.js
    script.js corrigido

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 function playRockPaperScissor(player1, player2) {
-    if (player1 == "Pedra" && player2 == "Pedra") {
+    if ((player1 == "Pedra" && player2 == "Pedra") || (player1 == "Papel" && player2 == "Papel") 
+    || (player1 == "Tesoura" && player2 == "Tesoura")) {
         return "Empate!";
-    } else if (player1 == "Pedra" && player2 == "Papel") {
-        return "Jogador 2 venceu!";
-    } else if (player1 == "Pedra" && player2 == "Tesoura") {
+    } else if ((player1 == "Pedra" && player2 == "Tesoura") || (player1 == "Papel" && player2 == "Pedra")
+    || (player1 == "Tesoura" && player2 == "Papel")) {
         return "Jogador 1 venceu!";
-    } else if (player1 == "Papel" && player2 == "Pedra") {
-        return "Jogador 1 venceu!";
-    } else if (player1 == "Papel" && player2 == "Papel") {
-        return "Empate!";
-    } else if (player1 == "Papel" && player2 == "Tesoura") {
+    } else if ((player1 == "Pedra" && player2 == "Papel") || (player1 == "Papel" && player2 == "Tesoura")
+    || (player1 == "Tesoura" && player2 == "Pedra")) {
         return "Jogador 2 venceu!";
-    } else if (player1 == "Tesoura" && player2 == "Pedra") {
-        return "Jogador 2 venceu!";
-    } else if (player1 == "Tesoura" && player2 == "Papel") {
-        return "Jogador 1 venceu!";
-    } else if (player1 == "Tesoura" && player2 == "Tesoura") {
-        return "Empate!";
     }
     return "Esta string não deveria ser retornada";
 }
