8800cca246ec76cfc62a955c384ec2e3f31fc9bc
rickzeppelin <rfalcetti@gmail.com>
2024-03-14 17:06:24 -0300

    Update script.js
    script.js atualizado

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 function playRockPaperScissor(player1, player2) {
-    if ((player1 == "Pedra" && player2 == "Pedra") || (player1 == "Papel" && player2 == "Papel") 
-    || (player1 == "Tesoura" && player2 == "Tesoura")) {
-        return "Empate!";
-    } else if ((player1 == "Pedra" && player2 == "Tesoura") || (player1 == "Papel" && player2 == "Pedra")
+    if ((player1 == "Pedra" && player2 == "Tesoura") || (player1 == "Papel" && player2 == "Pedra")
     || (player1 == "Tesoura" && player2 == "Papel")) {
         return "Jogador 1 venceu!";
     } else if ((player1 == "Pedra" && player2 == "Papel") || (player1 == "Papel" && player2 == "Tesoura")
     || (player1 == "Tesoura" && player2 == "Pedra")) {
         return "Jogador 2 venceu!";
     }
-    return "Esta string não deveria ser retornada";
+    return "Empate!";
 }
